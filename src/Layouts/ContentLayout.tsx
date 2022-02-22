42e9508351ef29be3c30e520e8ed6619db18771e
0too <rucu.oktavian@mail.ru>
2022-02-22 19:11:48 +0200

    add layout & card

---
+++ src/Layouts/ContentLayout.tsx
@@ -0,0 +1,16 @@
+import {Layout} from 'antd'
+
+
+const {Header} = Layout
+
+const information = [{
+  id: 1,
+  title: 'Title',
+  description: 'Some more text'
+}]
+
+export const ContentLayout = () => {
+    return (
+      <div></div>
+    )
+ }
