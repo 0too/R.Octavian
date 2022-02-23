50a6e3c821e5d56bceb298f78b410e069eba9f1a
0too <rucu.oktavian@mail.ru>
2022-02-23 17:11:10 +0200

    added card 4

--- src/Layouts/CardCustom.tsx
+++ src/Layouts/CardCustom.tsx
@@ -1,10 +1,34 @@
+import { Checkbox, Input } from 'antd'
 import Card from 'antd/lib/card'
+import { observer } from 'mobx-react-lite'
+import { useEffect } from 'react'
+import { IContentModel } from '../interfaces/interfaces'
 
 
-export const CardCustom = () => {
+export const CardCustom = observer(({ content }: { content: IContentModel }) => {
+
+    const { title, description, notes, changeNotes, isSeen, changeSeen, seen } = content
+
+
+    useEffect(() => {
+        if (isSeen) {
+            alert(`Is seen ${ title }`)
+        }
+    }, [ isSeen ])
+
     return (
-         <Card title="Card title" bordered={false}>
-          Card content
+        <Card title={ title } bordered={ false }>
+            { description }
+
+            <Input
+                value={ notes }
+                placeholder={ notes }
+                onChange={ (e) => changeNotes(e.target.value) }
+            />
+
+            <Checkbox defaultChecked={ seen } checked={ seen } onChange={ (e) => changeSeen(e.target.checked) } />
+
         </Card>
     )
-}
+
+})
--- src/Layouts/ContentLayout.tsx
+++ src/Layouts/ContentLayout.tsx
@@ -1,16 +1,37 @@
-import {Layout} from 'antd'
+import { Breadcrumb, Layout, Row } from 'antd'
+import { Content } from 'antd/lib/layout/layout'
+import { useRootStore } from '../index'
+import { IContentModel } from '../interfaces/interfaces'
+import { CardCustom } from './CardCustom'
 
-
-const {Header} = Layout
-
-const information = [{
-  id: 1,
-  title: 'Title',
-  description: 'Some more text'
-}]
+const { Footer } = Layout
 
 export const ContentLayout = () => {
+
+    const { contents } = useRootStore()
+
     return (
-      <div></div>
+        <>
+            <Content style={ { padding: '0 50px' } }>
+                <Breadcrumb style={ { margin: '16px 0' } }>
+                    <Breadcrumb.Item>Home</Breadcrumb.Item>
+                    <Breadcrumb.Item>List</Breadcrumb.Item>
+                    <Breadcrumb.Item>App</Breadcrumb.Item>
+                </Breadcrumb>
+                <div className='site-layout-content'>
+                    <Row gutter={ 16 }>
+                        { contents.map((content: IContentModel) => {
+                            return (
+                                <CardCustom key={ content.id } content={ content } />
+                            )
+                        }) }
+
+                    </Row>
+
+                </div>
+            </Content>
+            <Footer style={ { textAlign: 'center' } }>Ant Design ©2018 Created by Ant UED</Footer>
+        </>
+
     )
- }
+}
